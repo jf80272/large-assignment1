2c31477783cc34fa3ccaa0dcfa649c3f8632f3df
jf80272 <166781698+jf80272@users.noreply.github.com>
2025-02-22 14:42:31 -0700

    Update LibraryModel.java
    Changed name of instance variable playListList to playlistList

--- LibraryModel.java
+++ LibraryModel.java
@@ -5,5 +5,5 @@ import java.util.ArrayList;
 public class LibraryModel {
 	private ArrayList<Album> albumList;
 	private ArrayList<Song> songList;
-	private ArrayList<Playlist> playListList;
+	private ArrayList<Playlist> playlistList;
 }
