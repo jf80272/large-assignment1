529bff7706ac50406e62fa60a09e83d74916fdc7
JoyD-0303 <148120294+JoyD-0303@users.noreply.github.com>
2025-02-23 12:45:18 -0700

    Update LibraryModel.java
    Added Methods:
    - add Song
    - add Album
    - create Playlist (additional functions of adding and removing songs from playlist)
    - Rate song

--- LibraryModel.java
+++ LibraryModel.java
@@ -21,6 +21,85 @@ public class LibraryModel {
 	}
 	
 	/* METHODS */
+	// Add Song to songList
+	public void addToSongList(Song song) {
+		if (!this.containsSong(song)) {
+			Song s = new Song(song.getTitle(), song.getArtist(), song.getArtist());
+			s.setRating(song.getRating());
+			s.setFav(song.isFav());
+			this.songList.add(s);
+		}
+	}
+
+	// Add an album to albumList
+	public void addToAlbumList(Album album) {
+		Album al = new Album(album.getTitle(), album.getArtist(), album.getGenre(), album.getYear(),
+				album.getSongList());
+		for (Song song : al.getSongList()) {
+			if (!this.containsSong(song)) {
+				this.songList.add(song);
+			}
+		}
+		this.albumList.add(al);
+	}
+
+	// Private contains method to see if song is in songList
+	private boolean containsSong(Song song) {
+		for (Song s : this.songList) {
+			if (s.getTitle() == song.getTitle() &&
+					s.getArtist() == song.getArtist()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Create a playlist
+	public void createPlaylist(String name) {
+		if (!this.containsPlaylist(name)) {
+			this.playlistList.add(new Playlist(name));
+		}
+	}
+
+	// private contains method that checks if a playlist name is in playlistList
+	private boolean containsPlaylist(String name) {
+		for (Playlist playlist : this.playlistList) {
+			if (playlist.getName() == name) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Add a song to a playlist
+	public void addToPlaylist(Song song, String name) {
+		if (!this.containsSong(song)) {
+			this.addToSongList(song);
+		}
+		for (Playlist playlist : this.playlistList) {
+			if (playlist.getName() == name) {
+				playlist.addSong(song);
+			}
+		}
+	}
+
+	// Remove a song from a playlist
+	public void removeFromPlaylist(Song song, String name) {
+		for (Playlist playlist : this.playlistList) {
+			if (playlist.getName() == name) {
+				playlist.rmSong(song);
+			}
+		}
+	}
+	
+	public void rateSong(Song song , Rate rate) {
+		for (Song s : this.songList) {
+			if (s.getTitle() == song.getTitle() &&
+					s.getArtist() == song.getArtist()) {
+				s.setRating(rate);
+			}
+		}
+	}
 	/* GETTERS */
 	// List of albums
 	public ArrayList<String> getAlbumList() {
