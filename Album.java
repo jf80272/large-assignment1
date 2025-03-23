212a1d624ba3d8d10865ed0b9691c2037188f75f
JoyD-0303 <148120294+JoyD-0303@users.noreply.github.com>
2025-03-23 12:23:54 -0700

    Update Album.java
    Added contains method

--- Album.java
+++ Album.java
@@ -8,36 +8,36 @@ public class Album {
 	private final String artist;
 	private final String genre;
 	private final int year;
-	private final ArrayList<Song> songList;
-	
+	private ArrayList<Song> songList;
+
 	/* CONSTRUCTORS */
 	public Album(String title, String artist, String genre, int year, ArrayList<Song> songs) {
-        this.title = title;
-        this.artist = artist;
-        this.genre = genre;
-        this.year = year;
-        this.songList = songs;
-    }
-	
+		this.title = title;
+		this.artist = artist;
+		this.genre = genre;
+		this.year = year;
+		this.songList = songs;
+	}
+
 	/* METHODS */
-	
+
 	/* GETTERS */
 	public String getTitle() {
 		return this.title;
 	}
-	
+
 	public String getArtist() {
 		return this.artist;
 	}
-	
+
 	public String getGenre() {
 		return this.genre;
 	}
-	
+
 	public int getYear() {
 		return this.year;
 	}
-	
+
 	public ArrayList<Song> getSongList() {
 		ArrayList<Song> sList = new ArrayList<Song>();
 		for (Song song : this.songList) {
@@ -45,11 +45,20 @@ public class Album {
 		}
 		return sList;
 	}
-	
+
+	// Contain method
+	public boolean contains(String title) {
+		for (Song song : this.songList) {
+			if (song.getTitle().equals(title)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	// To String method
 	public String toString() {
-		String str = this.title + ", " + this.artist + ", " 
-				 + this.genre + ", " + this.year + "\n";
+		String str = this.title + "_" + this.artist + "_" + this.genre + "_" + this.year + "\n";
 		for (Song song : this.songList) {
 			str += song.getTitle() + "\n";
 		}
